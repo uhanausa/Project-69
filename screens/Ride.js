1879442c59dcd26cc427e080b19ab01d288a2120
uhanausa <95511515+uhanausa@users.noreply.github.com>
2022-10-07 19:03:00 -0700

    Add files via upload

--- screens/Ride.js
+++ screens/Ride.js
@@ -1,11 +1,92 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default class RideScreen extends Component {
+export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    //const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    //const { status } = Permissions.askAsync(Permissions.CAMERA);
+    //const { status } = await Permissions.askAsync(Permissions);
+    //const { status } = await Permissions.askAsync(CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    });
+  };
+
   render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    if (domState === "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Ride Screen</Text>
+        <Text style={styles.text}>
+          {hasCameraPermissions ? scannedData : "Request for Camera Permission"}
+        </Text>
+        
+        {/*
+          <TouchableOpacity
+            style={[styles.button, { marginTop: 25 }]}
+            onPress= this.getCameraPermissions("scanner")
+          >
+            <Text style={styles.buttonText}>Scan QR Code</Text>
+          </TouchableOpacity> 
+        */}
+        {/*
+          <TouchableOpacity
+            style={[styles.button, { marginTop: 25 }]}
+            onPress={() => this.getCameraPermissions("scanner")}
+          >
+            <Text style={styles.buttonText}>Scan QR Code</Text>
+          </TouchableOpacity> 
+        */}
+        {/*
+          <TouchableOpacity
+            style={[styles.button, { marginTop: 25 }]}
+            onPress={() => this.getCameraPermissions()}
+          >
+            <Text style={styles.buttonText}>Scan QR Code</Text>
+          </TouchableOpacity> 
+        */}
+
+        {/*
+          <TouchableOpacity
+            style={[styles.button, { marginTop: 25 }]}
+            onPress={() => this.getCameraPermissions("scanner")}
+          />
+        */}
+       
       </View>
     );
   }
@@ -20,6 +101,19 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#4C5D70",
-    fontSize: 30
+    fontSize: 15
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FBE5C0",
+    borderRadius: 15,
+    borderWidth: 2
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#4C5D70"
   }
 });
